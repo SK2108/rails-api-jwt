f6b31d8ec8b66d3c96813fe43687785ae5b9532c
SK2108 <85369151+SK2108@users.noreply.github.com>
2022-08-22 10:55:36 +0200

    Adding project files

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resource :user, only: [:create]
+  post "/login", to: "users#login"
+  get "/auto_login", to: "users#auto_login"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+end
